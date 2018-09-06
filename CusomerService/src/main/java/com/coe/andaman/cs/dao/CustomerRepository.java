09cbf5bc0e11d0ca76fd3132a5a01805bc87b9ba
Istiaque Hussain <Istiaque_hussain@apple.com>
2018-09-06 12:20:06 +0530

    Poject name changed

--- CusomerService/src/main/java/com/coe/andaman/cs/dao/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.coe.andaman.cs.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.coe.andaman.cs.entity.Customer;
-
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-
-}
